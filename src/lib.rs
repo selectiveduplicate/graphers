a71275026f4f884d7ac79099c5188da600127719
Abu Sakib <mabusakib@gmail.com>
2025-02-26 22:37:25 +0600

    Initial impl of graph objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Debug};
+use std::collections::HashMap;
 
 pub struct Edge {
     from_node: u32,
@@ -16,13 +16,13 @@ impl Edge {
     }
 }
 
-pub struct Node<T: Debug> {
+pub struct Node<T> {
     pub idx: u32,
     pub edges: HashMap<u32, Edge>,
     pub label: Option<T>,
 }
 
-impl<T: Debug> Node<T> {
+impl<T> Node<T> {
     /// Creates a new node without label.
     /// # Example
     /// ```
@@ -75,9 +75,14 @@ impl<T: Debug> Node<T> {
         self.edges.get(&neighbor)
     }
     /// Connects an edge between this node and the `neighbor` node.
-    /// Returns the new edge object.
+    /// If the source node already has an edge to a neighbor node, 
+    /// it updates the edge data and returns the old edge value. 
+    /// Otherwise returns `None`.
     pub fn add_edge(&mut self, neighbor: u32, weight: f32) -> Option<Edge> {
         let new_edge = Edge::new(self.idx, neighbor, weight);
+        eprintln!("{:?}", new_edge.from_node);
+        eprintln!("{:?}", new_edge.to_node);
+        eprintln!("{:?}", new_edge.weight);
         self.edges.insert(neighbor, new_edge)
     }
     /// Removes an edge between two nodes.
@@ -88,12 +93,49 @@ impl<T: Debug> Node<T> {
     }
 }
 
+pub struct Graph<T> {
+    pub capacity: usize,
+    pub nodes: Vec<Node<T>>,
+    pub undirected: bool,
+}
+
+impl<T> Graph<T> {
+    /// Creates a new grpah with the given capacity and directional type.
+    pub fn new(capacity: usize, undirected: bool) -> Self {
+        Self {
+            capacity,
+            nodes: Vec::with_capacity(capacity),
+            undirected,
+        }
+    }
+    /// Inserts an edge between two nodes in the graph.
+    pub fn insert_edge(&mut self, from: u32, to: u32, weight: f32) -> Option<Edge> {
+        if let Some(src_node_idx) = self.nodes.iter().position(|n| n.idx == from) {
+            self.nodes[src_node_idx].add_edge(to, weight)
+        } else {
+            //TODO: None is returned for two success cases, so should do something 
+            //like make an error enum when None is returned for nonexistent 
+            //nodes like here
+            None
+        }
+        //let src_node_idx = self.nodes.partition_point(|node| node.idx == from) - 1;
+    }
+    pub fn insert_node(&mut self, node: Node<T>) -> bool {
+        if self.nodes.len() == self.capacity {
+            false
+        } else {
+            self.nodes.push(node);
+            true
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_sorted_edge_list() {
+    fn test_creating_nodes_and_edges() {
         let mut node_10 = Node::<()>::new(10);
         let node_20 = Node::<()>::new(20);
         let node_30 = Node::<()>::new(30);
@@ -108,5 +150,36 @@ mod tests {
         assert_eq!(edge.from_node, 10);
         assert_eq!(edge.to_node, 20);
         assert_eq!(edge.weight, 24.33);
+    }
+
+    #[test]
+    fn test_creating_labelled_nodes() {
+        let n10_label = String::from("Furniture");
+        let node_10: Node<String> = Node::with_label(10, n10_label);
+        assert_eq!(node_10.label.unwrap(), String::from("Furniture"));
+    }
+
+    #[test]
+    fn test_creating_graph_objects_with_non_labelled_nodes() {
+        let graph: Graph<()> = Graph::new(5, false);
+        assert_eq!(graph.capacity, 5);
+    }
+
+    #[test]
+    fn test_creating_graph_objects_with_labelled_nodes() {
+        let mut graph: Graph<String> = Graph::new(5, false);
+        assert_eq!(graph.capacity, 5);
+
+        let node_20: Node<String> = Node::with_label(20, String::from("Furniture"));
+        let node_30: Node<String> = Node::with_label(30, String::from("Laptop"));
+        let node_40: Node<String> = Node::with_label(40, String::from("Clock"));
+
+        graph.insert_node(node_20);
+        graph.insert_node(node_30);
+        graph.insert_node(node_40);
+        
+        let edge = graph.insert_edge(40, 20, 1012.10);
+        assert!(edge.is_none());
+        assert_eq!(graph.nodes.len(), 3);
     }
 }
